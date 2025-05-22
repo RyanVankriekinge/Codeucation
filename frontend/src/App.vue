0d700e6daf1a0875d99c5481b03526c94085c353
Ryan Vankriekinge <ryan.vankriekinge@student.ehb.be>
2025-05-22 22:14:52 +0200

    feat(frontend): Create home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,6 +16,32 @@
         </nav>
       </div>
     </div>
+    <div v-if="isHome" class="nav-wave">
+      <div class="outer-wrapper desktop-nav">
+        <div class="wrapper">
+          <div class="section introduction">
+            <div class="column50">
+              <p class="introduction-subtitle">Lesgeven met vertrouwen dankzij leerpaden en geautomatiseerde code testing</p>
+              <p class="introduction-title">Leer programmeren met directe feedback!</p>
+              <button>Ga aan de slag!</button>
+            </div>
+            <div class="column50">
+              <div id="placeholder" style="width:100%; height:100%; background:#ccc; text-align:center; font-size:20px; cursor:pointer; line-height: 8; border-radius: 20px;">
+                <p style="margin: auto; height: 100%;">Video placeholder</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <svg viewBox="0 0 1440 200" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0,0 A720,70 0 0,0 1440,0 L1440,0 L0,0 Z" fill="#031F67" />
+      </svg>
+    </div>
+    <div v-else>
+      <svg viewBox="0 0 1440 200" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0,0 A720,20 0 0,0 1440,0 L1440,0 L0,0 Z" fill="#031F67" />
+      </svg>
+    </div>
   </header>
   <router-view>
   </router-view>
@@ -29,5 +55,10 @@
 </template>
 
 <script setup>
+  import { useRoute } from 'vue-router'
+  import { computed } from 'vue'
 
+  const route = useRoute()
+  console.log('ROUTE:', route)
+  const isHome = computed(() => route?.name === 'home')
 </script>

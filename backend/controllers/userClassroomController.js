bbbda00d8f7f56fa89c8f8c9abaeaffbaf3c3e0d
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-06-10 17:04:52 +0200

    feat(backend): Find classrooms by user

--- backend/controllers/userClassroomController.js
+++ backend/controllers/userClassroomController.js
@@ -13,3 +13,26 @@ exports.addUserToClassroom = async (req, res) => {
         res.status(500).json({ success: false, error: err.message });
     }
 };
+
+exports.getClassroomsForUser = async (req, res) => {
+    try {
+        const db = getDB();
+        const userId = req.params.userId;
+
+        if (!ObjectId.isValid(userId)) {
+            return res.status(400).json({ success: false, message: 'Invalid user ID' });
+        }
+        const userClassrooms = await db.collection('User_Classroom').find({ userId: new ObjectId(userId) }).toArray();
+
+        if (userClassrooms.length === 0) {
+            return res.json([]);
+        }
+
+        const classroomIds = userClassrooms.map(uc => new ObjectId(uc.classroomId));
+
+        const classrooms = await db.collection('Classrooms').find({_id: { $in: classroomIds }}).toArray();
+        res.json(classrooms);
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+};

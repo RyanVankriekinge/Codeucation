3f46757b09ceaf1ae6f00fc47838fa99e40dde40
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-07-15 14:35:07 +0200

    feat(backend): Include instruction, starter code and test file in exercise model

--- backend/models/Exercise.js
+++ backend/models/Exercise.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
     chapterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chapter', required: true },
-    title: { type: String, required: true }
+    title: { type: String, required: true },
+    instruction: { type: String, required: true },
+    starterCode: { type: String, required: true },
+    testFile: { type: String, required: true }
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);

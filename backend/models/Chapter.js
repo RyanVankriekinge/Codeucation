de1d9ebca85f72d7e517a25d7fd3f07d13670fab
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-24 21:54:05 +0200

    feat(backend): add course name and theory to getChapterById

--- backend/models/Chapter.js
+++ backend/models/Chapter.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const chapterSchema = new mongoose.Schema({
     courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
     title: { type: String, required: true },
-    orderIndex: { type: Number, required: true }
+    orderIndex: { type: Number, required: true },
+    theory: { type: String },
 });
 
 const Chapter = mongoose.model('Chapter', chapterSchema);

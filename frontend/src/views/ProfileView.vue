bb25178be633b1370934c2d0646d18f4300473a7
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-28 01:31:14 +0200

    feat(frontend): use profile component on profile page

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -2,35 +2,18 @@
   <main>
     <div class="outer-wrapper">
       <div class="wrapper">
-        <button class="big-button" @click="logout">
-          Uitloggen
-        </button>
+        <div class="section">
+          <div class="column66">
+
+          </div>
+          <ProfileLogOut />
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import ProfileLogOut from '../components/ProfileLogOut.vue';
 
-const router = useRouter();
-
-async function logout() {
-  try {
-    const response = await fetch('http://localhost:3000/api/users/logout', {
-      method: 'POST',
-      credentials: 'include',
-    });
-    const result = await response.json();
-    if (result.success) {
-      alert('Succesvol uitgelogd!');
-      router.push('/');
-    } else {
-      alert('Uitloggen mislukt. Probeer opnieuw.');
-    }
-  } catch (error) {
-    console.error('Logout error:', error);
-    alert('Er is een fout opgetreden bij het uitloggen.');
-  }
-}
 </script>

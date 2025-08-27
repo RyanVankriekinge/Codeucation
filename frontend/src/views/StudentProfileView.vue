4f2775eaaaa69a808e97480b76e56432acf27b59
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-27 18:27:06 +0200

    feat(frontend): Creating chapter containers on student profile page

--- frontend/src/views/StudentProfileView.vue
+++ frontend/src/views/StudentProfileView.vue
@@ -3,8 +3,10 @@
     <div class="outer-wrapper">
       <div class="wrapper">
         <div class="section">
-          <div class="column66">
-            <h1 style="margin-bottom: 10px;">{{ user.firstname || 'Naam' }} {{ user.name || 'Leerling' }}</h1>
+          <div class="full-width">
+            <h1 style="margin-bottom: 10px;">
+              {{ user.firstname || 'Naam' }} {{ user.name || 'Leerling' }}
+            </h1>
             <label for="courseSelect" class="paragraph" style="margin-right: 10px;">Selecteer cursus:</label>
             <div class="custom-select">
               <select v-model="currentCourseId" id="courseSelect" @focus="isOpen = true" @blur="isOpen = false">
@@ -22,11 +24,22 @@
               </svg>
             </div>
 
-            <div v-if="currentCourse">
-              <div v-for="chapter in currentCourse.chapters" :key="chapter._id" class="chapter-section">
-                <h3>{{ chapter.title }}</h3>
+            <div v-if="currentCourse" style="width: 100%;">
+              <div v-for="chapter in currentCourse.chapters" :key="chapter._id" class="chapter-container">
+                <div class="chapter-header" @click="chapter.open = !chapter.open">
+                  <h3>{{ chapter.title }}</h3>
+                  <svg v-if="!chapter.open" xmlns="http://www.w3.org/2000/svg" width="20" height="12"
+                    viewBox="0 0 44.047 23.523">
+                    <path d="M6,13.5,25.9,33.4,45.8,13.5" transform="translate(-3.879 -11.379)" fill="none"
+                      stroke="#031F67" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
+                  </svg>
+                  <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="12" viewBox="0 0 44.047 23.523">
+                    <path d="M6,33.4,25.9,13.5,45.8,33.4" transform="translate(-3.879 -12)" fill="none" stroke="#031F67"
+                      stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
+                  </svg>
+                </div>
 
-                <div class="exercise-list">
+                <div v-show="chapter.open" class="exercise-list">
                   <div v-for="exercise in chapter.exercises || []" :key="exercise._id" class="exercise-list-item"
                     @click="goToExercise(currentCourse._id, chapter._id, exercise._id)">
                     <h4 class="exercise-title">{{ exercise.title }}</h4>
@@ -35,14 +48,14 @@
                       <div class="naar-oefening">
                         <p>Naar oefening</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="9" height="17" viewBox="0 0 18.621 34.243">
-                          <path id="Icon_akar-chevron-right" data-name="Icon akar-chevron-right" d="M12,6,27,21,12,36"
-                            transform="translate(-9.879 -3.879)" fill="none" stroke="#707070" stroke-linecap="round"
+                          <path d="M12,6,27,21,12,36" fill="none" stroke="#707070" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="3" />
                         </svg>
                       </div>
                     </div>
                   </div>
                 </div>
+
               </div>
             </div>
 
@@ -67,7 +80,6 @@ const courses = ref([])
 const currentCourseId = ref(null)
 const isOpen = ref(false)
 
-
 const fetchCourses = async () => {
   try {
     const { data } = await axios.get(
@@ -77,6 +89,10 @@ const fetchCourses = async () => {
     if (data.success) {
       user.value = data.user
       courses.value = data.courses
+      courses.value.forEach(course => {
+        course.chapters.forEach(chapter => chapter.open = false)
+      })
+
       if (courses.value.length) currentCourseId.value = courses.value[0]._id
     }
   } catch (err) {
@@ -96,8 +112,23 @@ onMounted(fetchCourses)
 </script>
 
 <style scoped>
-.chapter-section {
-  margin-bottom: 40px;
+.chapter-container {
+  margin-bottom: 20px;
+  background-color: #e6f0ff;
+  border-radius: 10px;
+  width: 100%;
+}
+
+.chapter-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.chapter-header h3{
+  margin: auto 20px;
 }
 
 .exercise-list-item {
@@ -106,7 +137,7 @@ onMounted(fetchCourses)
   padding: 10px;
   border-radius: 10px;
   background-color: #f0f4ff;
-  margin-bottom: 10px;
+  margin: 5px 10px;
   cursor: pointer;
 }
 
